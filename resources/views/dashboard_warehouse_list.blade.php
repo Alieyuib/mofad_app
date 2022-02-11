9258d27bcaa898d7ea5ea363a9895e5b83414d51
Lexx Luey <71068965+LexxLuey@users.noreply.github.com>
2022-02-11 18:51:38 +0100

    Clean Up
    Removed unnecessary files from index

--- resources/views/dashboard_warehouse_list.blade.php
+++ resources/views/dashboard_warehouse_list.blade.php
@@ -5,7 +5,7 @@
 @endsection()
 
 @section('side_nav')
-@parent()
+    @parent()
 @endsection
 
 @section('top_nav')
@@ -14,51 +14,51 @@
 
 @section('content')
 
-<div class="row">
-    <div class="col s12 m12 l12">
-        <div class="card ">
-            <div class="card-content ">
-                <h4 class="header"></h4>
-                <p>Select warehouse</p>
-                <div id="view-avatar">
-                    <div class="row">
-                        <div class="col s8">
-                            <ul class="collection">
-                                @foreach($warehouse_list as $warehouse)
-                                <a class="" href="{{url('/dashboard/warehouse/'.$warehouse->id)}}">
-                                <li class="collection-item avatar ">
-                                    <img src="{{asset('assets/img/lubricant-image-1.jpg')}}" alt="" class="circle">
-                                    <span class="title">{{$warehouse->name}}</span>
-                                    <p> Open
-                                    </p>
-                                    <a href="#!" class="secondary-content">
-                                        <i class="material-icons">store</i>
-                                    </a>
-                                </li>
-                                </a>
-                                @endforeach
-                                
-                            </ul>
+    <div class="row">
+        <div class="col s12 m12 l12">
+            <div class="card ">
+                <div class="card-content ">
+                    <h4 class="header"></h4>
+                    <p>Select warehouse</p>
+                    <div id="view-avatar">
+                        <div class="row">
+                            <div class="col s8">
+                                <ul class="collection">
+                                    @foreach ($warehouse_list as $warehouse)
+                                        <a class="" href="{{ url('/dashboard/warehouse/' . $warehouse->id) }}">
+                                            <li class="collection-item avatar ">
+                                                <img src="{{ asset('assets/img/lubricant-image-1.jpg') }}" alt=""
+                                                    class="circle">
+                                                <span class="title">{{ $warehouse->name }}</span>
+                                                <p> Open
+                                                </p>
+                                                <a href="#!" class="secondary-content">
+                                                    <i class="material-icons">store</i>
+                                                </a>
+                                            </li>
+                                        </a>
+                                    @endforeach
+
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    <div>
-</div>
+            <div>
+            </div>
 
-                    
-                        
-                        
 
-        
-@endsection
 
-@section('footer')
-@parent()
-@endsection
 
-@section('footer_scripts')
-@parent()
-@endsection
-    
+
+
+        @endsection
+
+        @section('footer')
+            @parent()
+        @endsection
+
+        @section('footer_scripts')
+            @parent()
+        @endsection
